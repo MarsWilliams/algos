472482f2f6bbcb528c256b6647f97b7c99c85e55
Mars Williams <marswilliams@Marss-MacBook-Air.local>
2020-12-25 17:19:10 -0500

    updated to use generator to reduce space

--- 25_12_2020_single_number.py
+++ 25_12_2020_single_number.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 
 def single_number(nums: List[int]) -> int:
-    return [k for k, v in Counter(nums).items() if v == 1][0]
+    return (k for k, v in Counter(nums).items() if v == 1).__next__()
 
 
 assert single_number([4, 1, 2, 1, 2]) == 4
