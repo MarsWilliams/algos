b2c1aa9d55d5da253c9f70de606232bceeed62b7
Mars Williams <marswilliams@Marss-MacBook-Air.local>
2020-12-25 17:01:56 -0500

    added implementation with sets since collections library is not commonly accepted in an interview

--- 25_12_2020_single_number.py
+++ 25_12_2020_single_number.py
@@ -7,3 +7,16 @@ def single_number(nums: List[int]) -> int:
 
 
 assert single_number([4, 1, 2, 1, 2]) == 4
+
+
+def single_number_set(nums: List[int]) -> int:
+    singles = set()
+    for i in nums:
+        if i in singles:
+            singles.remove(i)
+        else:
+            singles.add(i)
+    return singles.pop()
+
+
+assert single_number_set([4, 1, 2, 1, 2]) == 4
